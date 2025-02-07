596e214120490978e906c5ccfee962bad5fbbac3
Govaerts Yoan <yoangovaerts@gmail.com>
2025-02-07 20:50:24 +0100

    🏗️ Refactor: Remplacer la gestion de la modal par un nouveau script et nettoyer le code HTML

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+
+  
+  // DOM Elements
+  const modalbg = document.querySelector(".bground");
+  const modalBtn = document.querySelectorAll(".modal-btn");
+  const formData = document.querySelectorAll(".formData");
+  
+
+  function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  
+  // launch modal event
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  
+  // launch modal form
+  function launchModal() {
+    modalbg.style.display = "block";
+  }
+  
+  
+// close modal form
+document.querySelector(".close").addEventListener("click", () => {
+    modalbg.style.display = "none";
+});
